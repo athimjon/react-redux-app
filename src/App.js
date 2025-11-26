b701c1289d52b42a1d90bd661697a55314f69714
athimjon <tonym5one@gmail.com>
2025-11-26 10:31:55 +0500

    added Browser Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Login, Main, Register } from './components/index'
 
 const App = () => {
 	return (
 		<div>
-			<BrowserRouter>
-				<Routes>
-					<Route path='/' element={<Main />} />
-					<Route path='/register' element={<Register />} />
-					<Route path='/login' element={<Login />} />
-				</Routes>
-			</BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='/login' element={<Login />} />
+			</Routes>
 		</div>
 	)
 }
