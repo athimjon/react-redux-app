079958b86d02ed1d5825313b8fd9bf34e2f030a4
athimjon <tonym5one@gmail.com>
2025-11-30 12:56:02 +0500

    Article UI 100%

--- src/components/article-detail.jsx
+++ src/components/article-detail.jsx
@@ -8,6 +8,7 @@ import {
 	getArticleDetailStart,
 	getArticleDetailSuccess,
 } from '../slice/article'
+import { Loader } from '../ui/index'
 
 const ArticleDetail = () => {
 	const { slug } = useParams()
@@ -31,20 +32,58 @@ const ArticleDetail = () => {
 
 	// ⛔ FIX: prevent "Cannot read properties of null"
 
-	return (
-		articleDetail !== null && (
+	return isLoading ? (
+		<Loader />
+	) : (
+		articleDetail != null && (
 			<div>
-				<div className='p-5 mb-4 bg-body-tertiary rounded-3'>
+				<div className='py-3 mb-4 rounded-3'>
 					<div className='container-fluid py-5'>
 						<h1 className='display-5 fw-bold'>{articleDetail.title}</h1>
 						<p className='col-md-8 fs-4'>{articleDetail.description}</p>
-						<p>
-							<span>Created at : </span>
+						<p className='text-muted'>
+							<span className='fw-bold'>Created at:</span>{' '}
 							{moment(articleDetail.createdAt).format('DD MMM, YYYY')}
 						</p>
-						<button className='btn btn-primary btn-lg' type='button'>
-							{`By ${articleDetail.author.username}`}
-						</button>
+
+						<div className='col-md-6'>
+							<div className='row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'>
+								<div className='col p-4 d-flex flex-column position-static'>
+									<strong className='d-inline-block mb-2 text-primary text-uppercase'>
+										{articleDetail.author.username}
+									</strong>
+									<p className='card-text mb-auto'>
+										{articleDetail.author.bio}
+									</p>
+								</div>
+
+								<div className='col-auto d-none d-lg-block'>
+									<svg
+										className='bd-placeholder-img'
+										width='200'
+										height='100%'
+										xmlns='http://www.w3.org/2000/svg'
+										role='img'
+										aria-label='Placeholder: Thumbnail'
+										preserveAspectRatio='xMidYMid slice'
+										focusable='false'
+									>
+										<title>Placeholder</title>
+										<rect width='100%' height='100%' fill='#55595c'></rect>
+										<text
+											x='45%'
+											y='53%'
+											fill='#fff'
+											className='fs-2 text-uppercase p-0 m-0'
+										>
+											{articleDetail.author.username[0]}
+										</text>
+									</svg>
+								</div>
+							</div>
+						</div>
+
+						<div>{articleDetail.body}</div>
 					</div>
 				</div>
 			</div>
